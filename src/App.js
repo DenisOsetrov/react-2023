9ec7aaf4a2d8561d36516b25005178bc8b6ceeee
DenisOsetrov <dvosetrov@gmail.com>
2023-04-18 12:33:15 +0300

    fix master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+// 1. Описати всю сім'ю сімпсонів (5 персонажів)
+//
+// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image)
+// з цього апі // https://rickandmortyapi.com/
+//     https://rickandmortyapi.com/api/character
+//        Створити 6 персонажів
 
-function App() {
+
+import Simpsons from "./components/Simpsons/Simpsons";
+
+const App = () => {
   return (
     <div>
-
+      <Simpsons/>
     </div>
   );
 }
