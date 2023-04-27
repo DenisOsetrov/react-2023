99d235aec334067c0d0f605be5c2b0c9b542e741
DenisOsetrov <dvosetrov@gmail.com>
2023-04-27 23:18:23 +0300

    lesson-3-1 part

--- src/service/axiosService.js
+++ src/service/axiosService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {baseUrl} from "../configs/url";
 
-const axiosService= axios.create({baseUrl:baseUrl}) // Якщо ключ і значення співпадає, то можна ({baseUrl})
+const axiosService= axios.create({baseURL:baseUrl}) // Якщо ключ і значення співпадає, то можна ({baseUrl})
 
 export {
     axiosService
