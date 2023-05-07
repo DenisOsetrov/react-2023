6ef6483b7275d8c9c3ee66be9570d0132d1e491c
DenisOsetrov <dvosetrov@gmail.com>
2023-05-08 02:29:16 +0300

    users-comments-car

--- src/validators/car.validator.ts
+++ src/validators/car.validator.ts
@@ -5,7 +5,7 @@ const carValidator = Joi.object({
         'string.pattern.base': 'Only letters min 1 max 20 ch.'
     }),
     price:Joi.number().min(0).max(1000000).required(),
-    year:Joi.number().min(1990).max(new Date().getFullYear()).required(),
+    year:Joi.number().min(1990).max(new Date().getFullYear()).required()
 })
 
 export {
