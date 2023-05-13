e9f9809f47119a0f51619734b9f92b7e797aaa5a
DenisOsetrov <dvosetrov@gmail.com>
2023-05-13 14:02:01 +0300

    Routes-TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+// В directory types - зберігаємо всі типи.
+//   - В ній створюємо useStates.type.ts - типізовані state
+//   - В ній створюємо axiosRes.type.ts
+
+
+
+
+
+
+
+
+
 import React from 'react';
 
 const App = () => {
